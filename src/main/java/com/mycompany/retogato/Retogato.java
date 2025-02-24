01fbe967fd5445d6abcb1363b03ceefe51675e79
EquipoDell <EquipoDell@192.168.0.164>
2025-02-24 16:03:15 -0600

    Interfaz del juego gato con diferentes funcionalides para una mejor presentacion alos usuarios que lo juegen.

---
+++ src/main/java/com/mycompany/retogato/Retogato.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.retogato;
+
+/**
+ *
+ * @author EquipoDell
+ */
+public class Retogato {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
